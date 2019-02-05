29464ce5030bd34f7dba1ec032a5030b9021c0d1
Sukanya Nath <8sukanya8@gmail.com>
2019-02-05 14:17:00 +0100

    added functions to extract frequency information in corpus processor

--- preprocess_NLP_pkg/corpus_processor.py
+++ preprocess_NLP_pkg/corpus_processor.py
@@ -8,6 +8,7 @@ from preprocess_NLP_pkg.load_data import read_file
 from nltk.probability import FreqDist
 from nltk import tokenize
 
+
 def author_dictionary (corpus_token_path, correct_author_path):
     """Take a path to a folder of token files and a file of correct authors
         and for each author, create a dictionary of words for all respective tokens
@@ -28,6 +29,7 @@ def author_dictionary (corpus_token_path, correct_author_path):
             if existing_token_files is not None:
                 author_name_token_dict[correct_author[i]].append(token_files[i])
     return author_name_token_dict
+
 
 def author_dictionary_italian(corpus_token_path):
     """Take a path to a folder of token files and a file of correct authors
@@ -50,24 +52,50 @@ def author_dictionary_italian(corpus_token_path):
                 author_name_token_dict[correct_author[i]].append(token_files[i])
     return author_name_token_dict
 
-def most_common_words_from_file(filepath, col_num = 1, word_separator = "\t",line_separator = "\n"):
-    lines = read_file(filepath).split(line_separator)
-    selected_column = []
-    for line in lines:
-        words = line.split(word_separator)
-        if(words.__len__() > col_num):
-            selected_column.append(words[col_num])
-    return selected_column
 
-
-def most_common_words_from_corpus(text, occurence = 2):
-    """Given a text, extract the types and their frequency, returns those types which have at least given number of occurences.
-        Say a word occurs twice, and occurence = 3, then the word is not selected.
+def get_freq_dist_from_corpus(text):
+    """Given a text, extract the types and their frequency
         Keyword arguments:
             text -- given text
-            occurence - those words are allowed which have a minimum occurence as specified
     """
     words = tokenize.word_tokenize(text.lower())
-    word_frequency = FreqDist(words)
-    selected_words = [key for key in word_frequency.keys() if word_frequency.get(key) >= occurence]
+    return FreqDist(words)
+
+
+def get_words_with_freq_at_least_n(text, n = 2):
+    """Given a list of documents, extracts a list of words having frequency greater than or equal to the occurrence mentioned
+        Keyword arguments:
+            doc_names_list -- list of document names
+            n - those words are selected which have frequency greater than or equal to n
+    """
+    word_freq_dists = get_freq_dist_from_corpus(text)
+    selected_words = [word for word,freq in word_freq_dists if freq >= n]
     return selected_words
+
+
+def get_complete_text(doc_list):
+    """Given a list of documents, returns the concatenated text contained in all of them
+        Keyword arguments:
+            text -- given text
+            window_size - number of characters in a window
+            step_size - number of characters to skip before beginning next windows
+    """
+    complete_text = ""
+    for doc in doc_list:
+        text = read_file( doc, mode='rb', ignore_comments= False).decode('utf-8')
+        complete_text = complete_text + text
+    return complete_text
+
+
+def append_folder_path_to_doc_list(doc_list, folder_path):
+    """Given a list of documents, returns the concatenated text contained in all of them
+            Keyword arguments:
+            text -- given text
+            window_size - number of characters in a window
+            step_size - number of characters to skip before beginning next windows
+    """
+    doc_paths =[]
+    for doc in doc_list:
+        doc_paths.append(folder_path + doc)
+    return  doc_paths
+
